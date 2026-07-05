fedf49c610cdf469a7f004411907b5dcd6952a78
YASSERRMD <arafath.yasser@gmail.com>
2026-07-05 22:28:54 +0400

    feat(core): export keyframes module from package entry point
    Re-exports Easing, ContinuousEasing, EASING_FUNCTIONS, Keyframe,
    KeyframeTrack, Property, isKeyframeTrack, compileKeyframeTrack,
    resolveProperty and its specializations, KeyframeValidationDiagnostic,
    and validateKeyframeTrack from @cadra/core's top-level entry point.

---
+++ packages/core/src/keyframes/index.ts
@@ -0,0 +1,13 @@
+export {
+  compileKeyframeTrack,
+  resolveColorProperty,
+  resolveNumberProperty,
+  resolveProperty,
+  resolveVector3Property,
+} from "./compile.js";
+export type { ContinuousEasing, Easing } from "./easing.js";
+export { EASING_FUNCTIONS } from "./easing.js";
+export type { Keyframe, KeyframeTrack, Property } from "./keyframe-track.js";
+export { isKeyframeTrack } from "./keyframe-track.js";
+export type { KeyframeValidationDiagnostic } from "./validate.js";
+export { validateKeyframeTrack } from "./validate.js";
