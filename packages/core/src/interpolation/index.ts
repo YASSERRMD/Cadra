63e2e5f15b8bfcef056a4d3a87a57c1cfdb53b96
YASSERRMD <arafath.yasser@gmail.com>
2026-07-05 22:09:56 +0400

    feat(core): export interpolation toolkit from package entry point
    Wires up the interpolation barrel (interpolate, spring, easing curves,
    cubicBezier, lerp/color/vector helpers, and their option/config/error types)
    via export * from ./interpolation/index.js, and updates the package doc
    comment to list interpolation as implemented rather than a later phase.

---
+++ packages/core/src/interpolation/index.ts
@@ -0,0 +1,25 @@
+export {
+  cubicBezier,
+  easeInBack,
+  easeInCubic,
+  easeInElastic,
+  easeInExpo,
+  easeInOutBack,
+  easeInOutCubic,
+  easeInOutElastic,
+  easeInOutExpo,
+  easeOutBack,
+  easeOutCubic,
+  easeOutElastic,
+  easeOutExpo,
+  linear,
+} from "./easing.js";
+export type { ExtrapolateMode, InterpolateOptions } from "./interpolate.js";
+export {
+  interpolate,
+  InterpolateRangeLengthMismatchError,
+  NonMonotonicInputRangeError,
+} from "./interpolate.js";
+export { interpolateColor, interpolateVector2, interpolateVector3, lerp } from "./lerp.js";
+export type { SpringConfig } from "./spring.js";
+export { spring } from "./spring.js";
